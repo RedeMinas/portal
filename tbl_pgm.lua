b65bfb3ed33977528c26d76a4a7ca44b964ffeea
Carlos Henrique G. Paulino <chgp@transmidia-01.redeminas.mg.gov.br>
2016-08-30 14:38:55 -0300

    now load external data from tbl_pgm.lua

---
+++ tbl_pgm.lua
@@ -0,0 +1,31 @@
+return {
+   [1] = { descricao =  "Uma revista eletrônica cultural sobre teatro, cinema, artes plásticas,\nliteratura e outras manifestações artísticas de forma aprofundada e \ncom um apurado cuiado estético e crítico."
+   },
+   [2] = { descricao =  "Uma revista eletrônica dedicada à musica pop e suas vertentes.Traz a \nhistória, das grandes bandas de rock, divulga artistas independentes, \ndestaca os lançamentos, fala tudo sobre instrumentos musicais e conta \nainda com entrevistas."
+   },
+   [3] = { descricao =  "O Arrumação abre espaço para diversas manifestações culturais como \na música, a dança, o teatro, a poesia, o folclore, as tradições, a \nnotícia boa e o bom-humor, apresentando os valores mais autênticos da \ncultura brasileira."
+   },
+   [4] = { descricao = "O Brasil das Gerais trata de temas como patrimônio cultural, tecnologia \nsocial e saúde. É sobre pessoas e sobre histórias de nossa Minas Gerais."
+   },
+   [5] = { descricao = "As aventuras dos bonecos Sdruvs, Judoca, Druzila e seus amigos, que \nvivenciam situações inusitadas, lúdicas e, por vezes, absurdas."
+   },
+   [6] = { descricao = "O programa traz reportagens, matérias e documentários com o objetivo \nde divulgar a música clássica de forma atraente."
+   },
+   [7]=  { descricao ="Magazine musical que exibe reportagens e entrevistas com grandes \nnomes do universo da música brasileira e internacional, combinando \nconteúdo informativo, opinativo, making offs e depoimentos de artistas \ne críticos."
+   },
+   [8] = { descricao = "A atração faz uma viagem pelo mundo dos livros e o universo que se \nabre a partir da palavra escrita, falada e cantada."
+   },
+   [9] = { descricao =  "Telejornal que leva aos mineiros as notícias de Minas e dá dicas de \nsaúde, educação, cultura e esporte. O programa traz entrevistas ao vivo, \nopinião de especialistas e reportagens especiais"
+   },
+   [10] = { descricao = "A notícia do futebol mineiro logo depois da rodada do fim de semana. \nMatérias especiais, compactos dos jogos, gols e os destaques do\n esporte no Brasil e no mundo. Informação com a participação de \nconvidados especiais e muito bom humor."
+   },
+   [11]= { descricao = "Mulhere-se é um programa construído de forma colaborativa que \npretende abordar a pluralidade do ser mulher e dar visibilidade às \nquestões e à construção social das imagens e do papel das mulheres."
+   },
+   [11] =  { descricao = "O Rede Mídia é um programa de debate que analisa e discute o papel da \n mídia nos dias de hoje, reafirmando o conceito da TV colaborativa."
+   },
+   [12] = { descricao =  "O Sou 60 busca abordar o envelhecimento de forma aberta e corajosa, \ncom seriedade e otimismo, discutindo temas como cidadania, saúde, \nsexualidade, comportamento, tecnologia, mercado de trabalho, viagem \ne exemplos inspiradores."
+   },
+   [13] = { descricao = "Programa dedicado à exibição de videoclipes. Exibe um panorama da \nmúsica pop atual e suas novas tendências, e revisita grandes bandas e \nartistas da história da música."
+   }
+
+}
